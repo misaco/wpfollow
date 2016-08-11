dfba28a8dae4ce06d16b02c1b8bbc91dc3e4bdd6
sadegh3333 <sadegh3333@ggmail.com>
2016-08-12 03:07:48 +0430

    change version number

--- wpfollow.php
+++ wpfollow.php
@@ -5,7 +5,7 @@ Plugin Name: wpfollow
 Plugin URI: http://misaco.ir
 Description: All you need is a plugin to your social networks on your site 
 Author: MiSaCo.
-Version: 0.0.1
+Version: 0.0.3
 Author URI: http://misaco.ir
 Text Domain: wpfollow
 */
